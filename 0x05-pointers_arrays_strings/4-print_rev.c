b5d1c1d658b0b9f50cf79fa748aa19a3b7595ed4
joshua <joshini586@gmail.com>
2022-10-24 17:02:03 -0700

    jk

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string
+ * @s: srinng
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[1]);
+
+	_putchar('\n');
+}
