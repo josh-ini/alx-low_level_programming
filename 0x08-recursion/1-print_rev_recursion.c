282ed1d70f3c40ef1109bc8d458e548e83827ec2
joshua <110654709+josh-ini@users.noreply.github.com>
2022-11-03 19:32:53 +0100

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+
+ * _print_rev_recursion - reverses a string
+
+ * @s: input
+
+ * Return: Always 0
+
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+
+	if (*s)
+
+	{
+
+		_print_rev_recursion(s + 1);
+
+		_putchar(*s);
+
+	}
+
+}
