e2f7020b1e7818ea48fcffdb1714b403ddc2172e
joshua <110654709+josh-ini@users.noreply.github.com>
2022-11-06 20:29:25 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
   * @argv: argument vector
   * Return: Always zero
   */ 
-int main(int argc, char *argv[])
-   (void) argc;
-{  
+int main(int argc, char *argv[]);   
+{ 
+        (void) argc;
         printf("%s\n", argv[0]);
 
         return (0);
