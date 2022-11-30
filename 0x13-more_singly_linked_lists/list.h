06ebaf97d6b02309ad2820c6c2e8e6a5ac02364a
joshua <110654709+josh-ini@users.noreply.github.com>
2022-11-30 04:57:04 +0100

    Update list.h

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,37 +1,38 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <string.h>
 
 /**
- * struct listint_t - singly link list
- * @n - integer
- * @next - points to the next node
- * description - singly link list 
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
-
-typedef struct listint_t
+typedef struct listint_s
 {
 	int n;
-	struct listint_t *next;
+	struct listint_s *next;
 } listint_t;
 
 /**
- * struct listptr_s - singly link list
- * @n - pointer of node
- * @next - points to the next node
- * description - singly link list of pointers
+ * struct listptr_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
  */
-
 typedef struct listptr_s
 {
 	void *p;
 	struct listptr_s *next;
-}
+} listptr_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -43,4 +44,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **);
+listint_t *find_listint_loop(listint_t *);
+void free_listint(listint_t *);
+void free_listint2(listint_t **);
 #endif
