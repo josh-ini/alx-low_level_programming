a212d882d2f0232561b14ef6068339613c0a4055
joshua <110654709+josh-ini@users.noreply.github.com>
2022-11-30 08:07:21 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,3 +1,34 @@
+#include "lists.h"
+
+/**
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: void
+ */
+void free_listp2(listptr_t **head)
+{
+	listptr_t *temp;
+	listptr_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a listint_t list safely
+ * @h: the list to free
+ *
+ * Return: no of nodes
+ */
 size_t free_listint_safe(listint_t **h)
 {
 	size_t nnodes = 0;
